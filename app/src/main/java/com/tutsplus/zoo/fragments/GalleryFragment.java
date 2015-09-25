9632ffa0448780329adb7f636b05bb5274e41d63
Arif A Mondal <arifali.mondal@gmail.com>
2015-09-26 00:04:30 +0300

    Gallery Grid

--- app/src/main/java/com/tutsplus/zoo/fragments/GalleryFragment.java
+++ app/src/main/java/com/tutsplus/zoo/fragments/GalleryFragment.java
@@ -4,8 +4,13 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
 
 import com.tutsplus.zoo.R;
+import com.tutsplus.zoo.adapters.GalleryImageAdapter;
 import com.tutsplus.zoo.models.GalleryImage;
 import com.tutsplus.zoo.utils.GalleryApiInterface;
 
@@ -21,14 +26,33 @@ import retrofit.client.Response;
  * Created by dreamer on 20-09-2015.
  */
 public class GalleryFragment extends Fragment {
+
+    private GridView mGridView;
+    private GalleryImageAdapter mAdapter;
+
     public static  GalleryFragment getInstance(){
         GalleryFragment fragment = new GalleryFragment();
         return fragment;
     }
 
     @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate( R.layout.fragment_gallery_grid, container, false );
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mGridView = (GridView) view.findViewById( R.id.grid );
+    }
+
+    @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState){
         super.onActivityCreated(savedInstanceState);
+
+        mAdapter = new GalleryImageAdapter(getActivity(), 0);
+        mGridView.setAdapter( mAdapter );
 
         RestAdapter restAdapter = new RestAdapter.Builder()
                 .setEndpoint(getString(R.string.gallery_feed))
@@ -44,7 +68,10 @@ public class GalleryFragment extends Fragment {
 
                 for( GalleryImage image : galleryImages ) {
                     Log.e("Zoo", image.getThumbnail());
+                    mAdapter.add(image);
                 }
+
+                mAdapter.notifyDataSetChanged();
             }
 
             @Override
